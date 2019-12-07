4f8ccda0fe7a3b018d48dfee55ea6da113331d9e
zomorodiyan <mehrdadzomorodiyan@gmail.com>
2019-12-07 03:31:31 +0330

    SolidFundation

---
+++ CheckutTests/test.cpp
@@ -0,0 +1,8 @@
+#include "pch.h"
+#include "../Supermarket/Checkout.cpp"
+
+TEST(TestCaseName, TestName) {
+	Checkout co;
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
---
+++ Supermarket/Checkout.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "Checkout.h"
+
+
+Checkout::Checkout()
+{
+}
+
+
+Checkout::~Checkout()
+{
+}
